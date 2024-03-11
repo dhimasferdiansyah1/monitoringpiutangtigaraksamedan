32fc6e415f1ca88297b12b921c92c3689557ee3e
Dhimas Ferdiansyah <starblackdest@gmail.com>
2024-03-11 18:38:02 +0700

    update auth

--- app/(landingPage)/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(landingPage)/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,25 @@
+import { Button } from "@/components/ui/button";
 import { SignUp } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs";
+import Link from "next/link";
 
-export default function Page() {
+export default async function Page() {
+  const { userId } = auth();
+  const user = await currentUser();
+
   return (
     <div className="flex justify-center min-h-screen pt-14">
-      <SignUp />
+      {userId && <SignUp />}
+      {!userId && (
+        <div className="flex flex-col gap-2 items-center justify-center">
+          <p className=" text-muted-foreground">
+            Akses terbatas, hanya bisa dilakukan oleh pemilik website!
+          </p>
+          <Link href="/">
+            <Button variant="default">Kembali</Button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
